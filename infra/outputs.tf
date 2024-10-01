0b374dd5afb06dd39363af0cb28504ad2eb0e99c
kreverson <kreversonjulho@gmail.com>
2024-10-01 18:04:31 -0300

    Ajustado locais

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -10,11 +10,6 @@ output "eks_cluster_certificate_authority_data" {
   value       = module.eks.cluster_certificate_authority_data
 }
 
-output "eks_cluster_token" {
-  value       = module.eks.cluster_token
-  sensitive   = true
-}
-
 output "eks_cluster_endpoint" {
   value       = module.eks.cluster_endpoint
 }
