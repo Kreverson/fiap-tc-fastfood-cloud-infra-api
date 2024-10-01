0cf85ffbeda5bd0276fd78f836eb29c244598682
kreverson <kreversonjulho@gmail.com>
2024-10-01 19:02:33 -0300

    Ajustado locais

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,21 +1,5 @@
 output "vpc_id" {
-  value       = aws_vpc.main_vpc.id
-}
-
-output "eks_cluster_id" {
-  value       = module.eks.cluster_id
-}
-
-output "eks_cluster_certificate_authority_data" {
-  value       = module.eks.cluster_certificate_authority_data
-}
-
-output "eks_cluster_endpoint" {
-  value       = module.eks.cluster_endpoint
-}
-
-output "eks_node_group_ids" {
-  value       = module.eks.node_groups["workers"].id
+  value    = aws_vpc.main_vpc.id
 }
 
 output "eks_private_subnet_ids" {
@@ -52,4 +36,36 @@ output "aws_nat_gateway_eks_id" {
 
 output "aws_private_route_table_eks_id" {
   value = aws_route_table.private.id
+}
+
+output "eks_cluster_endpoint" {
+  value       = aws_eks_cluster.eks_cluster.endpoint
+}
+
+output "eks_cluster_arn" {
+  value       = aws_eks_cluster.eks_cluster.arn
+}
+
+output "eks_cluster_certificate" {
+  value       = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+}
+
+output "eks_cluster_name" {
+  value       = aws_eks_cluster.eks_cluster.name
+}
+
+output "eks_node_group_name" {
+  value       = aws_eks_node_group.eks_nodes.node_group_name
+}
+
+output "eks_node_group_role_arn" {
+  value       = aws_iam_role.eks_node_role.arn
+}
+
+output "eks_node_group_scaling" {
+  value = {
+    desired_size = aws_eks_node_group.eks_nodes.scaling_config[0].desired_size
+    min_size     = aws_eks_node_group.eks_nodes.scaling_config[0].min_size
+    max_size     = aws_eks_node_group.eks_nodes.scaling_config[0].max_size
+  }
 }
